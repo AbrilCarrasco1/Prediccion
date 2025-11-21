834ac558d8c9e598cf416a065fa2fa29b863cd5a
AbrilCarrasco1 <93625482+AbrilCarrasco1@users.noreply.github.com>
2025-11-20 22:44:15 -0600

    Update Scores.py

--- Scores.py
+++ Scores.py
@@ -10,7 +10,7 @@ st.header('Datos')
 def user_input_features():
   # Entrada
   Horas_Estudiadas = st.number_input('Horas estudiadas:', min_value=0.0, max_value=100.0, value = 0.0, step = 1.0)
-  Horas_de_sueño = st.number_input('Horas de sueño:',  min_value=0.0, max_value=1.0, value = 0.0, step = 1.0)
+  Horas_de_sueño = st.number_input('Horas de sueño:',  min_value=0.0, max_value=100.0, value = 0.0, step = 1.0)
   Asistencia = st.number_input('Asistencia:', min_value=0.0, max_value=230.0, value = 0.0, step = 1.0)
   Calificacion_previa  = st.number_input('Calificación previa:', min_value=0.0, max_value=140.0, value = 0.0, step = 1.0)
 
